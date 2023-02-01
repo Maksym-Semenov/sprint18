c2d6699c0885356e31a3fad4417ab440d51ee471
DeathForRussia <ramirkiev@gmail.com>
2023-02-01 22:32:29 +0200

    end of work

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -14,6 +14,7 @@ namespace TaskAuthenticationAuthorization.Models
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        public string FirstName2 { get; set; }
         public string Address { get; set; }
         public Discount? Discount { get; set; }
         public ICollection<Order> Orders { get; set; }
